3f3fe490883b15ecf219f20f5a7d48bfee471bfb
Matias Bacelar <matiasbacelar@gmail.com>
2023-07-25 23:14:03 -0300

    Head component added

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,3 +1,15 @@
+import { Fragment } from 'react';
+import Head from '../Head';
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <Fragment>
+      <Head
+        title="Easybank"
+        description="Easebank is the next generation digital banking. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more."
+      />
+
+      <main>Home page</main>
+    </Fragment>
+  );
 }
